b6dd7e555e2483a8aaff54a1e90ab3467d43f78e
Rodolfo Belo <rsilva@casadasfechaduras.br>
2021-07-27 14:59:49 -0300

    bug form resolution

--- src/app/autenticacao/cadastro-pj/cadastro-pj.module.ts
+++ src/app/autenticacao/cadastro-pj/cadastro-pj.module.ts
@@ -4,6 +4,15 @@ import { CadastrarPjComponent } from './components/cadastrar-pj/cadastrar-pj.com
 import { CadastroPjComponent } from './components/cadastro-pj.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 
 
 
@@ -15,7 +24,15 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     FlexLayoutModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule
   ]
 })
 export class CadastroPjModule { }
